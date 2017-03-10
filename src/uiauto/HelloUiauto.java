e65113537b0997b15fa23af94c8ca0a1fb3a4ced
gitghought <simpleedu.hello@gmail.com>
2017-03-10 19:51:58 +0800

    20170310:添加测试方法：testOpenLauncherMenu，用于打开launcher，并点击菜单键

--- src/uiauto/HelloUiauto.java
+++ src/uiauto/HelloUiauto.java
@@ -1,6 +1,18 @@
 package uiauto;
 
+import java.io.IOException;
+
+import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class HelloUiauto extends UiAutomatorTestCase{
+	/**
+	 * Æô¶¯laucnehr
+	 * @throws IOException 
+	 */
+	public void testOpenLauncherMenu() throws IOException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		UiSelector selector = new UiSelector();
+	}
 }
