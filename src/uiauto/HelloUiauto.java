4a5ac2de84d77da1d82c52b6a31030a92cfe34d1
gitghought <simpleedu.hello@gmail.com>
2017-03-11 19:07:47 +0800

    20170311: 创建uiselector对象和uiobject对象，实现点击相应对象

--- src/uiauto/HelloUiauto.java
+++ src/uiauto/HelloUiauto.java
@@ -2,17 +2,28 @@ package uiauto;
 
 import java.io.IOException;
 
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
+import android.widget.TextView;
+
 public class HelloUiauto extends UiAutomatorTestCase{
+
 	/**
 	 * Æô¶¯laucnehr
 	 * @throws IOException 
+	 * @throws UiObjectNotFoundException 
 	 */
-	public void testOpenLauncherMenu() throws IOException {
+	public void testOpenLauncherMenu() throws IOException, UiObjectNotFoundException {
 		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
 		Runtime.getRuntime().exec(cmdStr);
 		UiSelector selector = new UiSelector();
+		selector.className("android.widget.TextView");
+		selector.packageName("com.cyanogenmod.trebuchet");
+		selector.index(2);
+		UiObject obj = new UiObject(selector);
+		obj.click();
 	}
 }
