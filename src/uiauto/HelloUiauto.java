61650671abe2b32ad477ab58b1b69ccd31b6161c
gitghought <simpleedu.hello@gmail.com>
2017-03-12 23:06:17 +0800

    20170312:实现通过非reourceid的方式打开launcher中的某个组件

--- src/uiauto/HelloUiauto.java
+++ src/uiauto/HelloUiauto.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class HelloUiauto extends UiAutomatorTestCase{
 
 	/**
-	 * 启动laucnehr
+	 * 启动laucnehr中的时间小组件
 	 * @throws IOException 
 	 * @throws UiObjectNotFoundException 
 	 */
@@ -24,6 +24,22 @@ public class HelloUiauto extends UiAutomatorTestCase{
 		UiSelector  sel = new UiSelector().className("android.widget.RelativeLayout").packageName("com.cyanogenmod.lockclock").index(0).resourceId("com.cyanogenmod.lockclock:id/digital_appwidget");
 		UiObject obj = new UiObject(sel);
 		obj.clickAndWaitForNewWindow();
+	}
+	
+	public void testOpenLauncher() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/workspace");
+		UiObject obj = new UiObject(sel);
+		//obj.clickAndWaitForNewWindow();
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(1);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
 	}
 
 }
