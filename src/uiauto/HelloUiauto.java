196a0820a6131ee62f9ca24fdb4e68f1d81812fc
gitghought <simpleedu.hello@gmail.com>
2017-03-12 23:24:21 +0800

    20170312:实现将手机中的四大天王和菜单键逐一点击开启的自动化测试脚本

--- src/uiauto/HelloUiauto.java
+++ src/uiauto/HelloUiauto.java
@@ -42,4 +42,87 @@ public class HelloUiauto extends UiAutomatorTestCase{
 		objchild.clickAndWaitForNewWindow();
 	}
 
+	public void testOpenLauncherAppCenter() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/workspace");
+		UiObject obj = new UiObject(sel);
+		//obj.clickAndWaitForNewWindow();
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(2);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+	public void testOpenLauncherPhone() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/hotseat");
+		UiObject obj = new UiObject(sel);
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(0);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+	public void testOpenLauncherMessage() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/hotseat");
+		UiObject obj = new UiObject(sel);
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(1);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+
+	public void testOpenLauncherMenu() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/hotseat");
+		UiObject obj = new UiObject(sel);
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(2);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+
+	public void testOpenLauncherBrowser() throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/hotseat");
+		UiObject obj = new UiObject(sel);
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(3);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+
+	public void testOpenLauncherCamera () throws IOException, UiObjectNotFoundException {
+		String cmdStr = "monkey -p com.cyanogenmod.trebuchet -v 1";
+		Runtime.getRuntime().exec(cmdStr);
+		
+		UiSelector  sel = new UiSelector().resourceId("com.android.launcher3:id/hotseat");
+		UiObject obj = new UiObject(sel);
+		int count = obj.getChildCount();
+		System.out.println("count = " + count);
+		
+		UiSelector  selchild = new UiSelector().className("android.widget.TextView").packageName("com.cyanogenmod.trebuchet").index(4);
+		UiObject  objchild = obj.getChild(selchild);
+		objchild.clickAndWaitForNewWindow();
+	}
+
 }
